3382132bd960560ea602826e0418786d9fe0534a
pcreyes9 <pcreyes09@gmail.com>
2025-08-14 11:32:37 +0800

    UPD LOGO

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -41,9 +41,7 @@
 <body class="index-page">
     <header id="header" class="header d-flex align-items-center fixed-top">
         <div class="container-fluid container-xl position-relative d-flex align-items-center">
-    
           <a href="{{route('home')}}" class="logo d-flex align-items-center me-auto">
-            <!-- Uncomment the line below if you also wish to use an image logo -->
             <img src="assets/template/logo/PSA-Logoo.png" alt="">
             {{-- <h1 class="sitename">PSA</h1> --}}
           </a>
