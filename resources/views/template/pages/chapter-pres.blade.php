81e368444d81d77e770f2806c21614d6e98c3591
Paul Cedrick Reyes <pcreyes09@gmail.com>
2026-01-29 10:04:56 +0800

    upd

--- resources/views/template/pages/chapter-pres.blade.php
+++ resources/views/template/pages/chapter-pres.blade.php
@@ -5,13 +5,13 @@
 @section('content')
     <section>
         <div class="container pt-5 mt-5">
-            <img src="images/Chapter-Presidents_Luzon2025-rev4.png" class="img-fluid animated" alt="">
+            <img src="images/Chapter-Presidents_Luzon.png" class="img-fluid animated" alt="">
         </div>
         <div class="container pt-5 mt-2">
-            <img src="images/Chapter-Presidents_Visayas2025-rev4.png" class="img-fluid animated" alt="">
+            <img src="images/Chapter-Presidents_Visayas.png" class="img-fluid animated" alt="">
         </div>
         <div class="container pt-5 mt-2">
-            <img src="images/Chapter-Presidents_Mindanao2025-rev4.png" class="img-fluid animated" alt="">
+            <img src="images/Chapter-Presidents_Mindanao.png" class="img-fluid animated" alt="">
         </div>
     </section>
 @endsection
