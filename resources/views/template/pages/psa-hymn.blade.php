b14020bf6706e07d5131ed7a5dd1eae3a46e00c1
Paul Cedrick Reyes <pcreyes09@gmail.com>
2026-01-30 10:35:20 +0800

    upd

--- resources/views/template/pages/psa-hymn.blade.php
+++ resources/views/template/pages/psa-hymn.blade.php
@@ -7,7 +7,7 @@
         <div class="container pt-5">
             <div class="mx-auto" style="max-width: 90%">
                 <div class="ratio ratio-16x9">
-                    <iframe src="https://www.youtube.com/embed/5SMk8MZjFPg?si=g_ZjMaWyMDrF3jFB"
+                    <iframe src="https://youtu.be/SPERPlRi7xo"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             allowfullscreen
