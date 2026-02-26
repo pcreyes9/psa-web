b86d6f64d07ff4433c154b715d7efbfab76a732a
pcreyes9 <pcreyes09@gmail.com>
2026-02-26 16:11:45 +0800

    upd psa hymn

--- resources/views/template/pages/psa-hymn.blade.php
+++ resources/views/template/pages/psa-hymn.blade.php
@@ -7,7 +7,7 @@
         <div class="container pt-5">
             <div class="mx-auto" style="max-width: 90%">
                 <div class="ratio ratio-16x9">
-                    <iframe src="https://www.youtube.com/embed/SPERPlRi7xo?si=ufdJQlH6BEK9dvPF"
+                    <iframe src="https://www.youtube.com/embed/hkIcSJ5enp8?si=tkrOkA55A5zfZmmB"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             allowfullscreen
