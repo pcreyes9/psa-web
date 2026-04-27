42488fa03854c81442ac96c0126b8bb92fac8b81
Paul Cedrick Reyes <pcreyes09@gmail.com>
2026-04-27 14:23:27 +0800

    upd workshop-reg

--- resources/views/template/pages/scientific-program.blade.php
+++ resources/views/template/pages/scientific-program.blade.php
@@ -12,23 +12,39 @@
                 WORKSHOP REGISTRATION
             </a>
         </div> --}}
-        @if (session('success'))
-            <div class="alert alert-success mt-4 text-center">
-                {{ session('success') }}
+        <div class="container section-title mt-5 pt-5" data-aos="fade-up">
+            <h2>SCIENTIFIC PROGRAM</h2>
+            <p>May 14, 2026 | KCC Events & Convention Center | General Santos City<br></p>
+            @if (session('success'))
+                <div class="alert alert-success mt-4 text-center">
+                    {{ session('success') }}
+                </div>
+            @endif
+            <div class="row my-4 pt-5 justify-content-center">
+                <div class="col-md-6 mb-2">
+                    <a href="{{ route('workshop-registration') }}"
+                        target="_blank"
+                        class="btn btn-primary w-100 px-4 py-2 fs-5 fw-bold text-center">
+                        Workshp Registration
+                    </a>
+                </div>
             </div>
-        @endif
+            <div class=" pt-3 mt-2">
+                <img src="images/sci-program/day1.png?v={{ time() }}" class="img-fluid animated" alt="">
+            </div>
+            <div class=" pt-sm-5 mt-2">
+                <img src="images/sci-program/day2.png?v={{ time() }}" class="img-fluid animated" alt="">
+            </div>
+            <div class=" pt-sm-5 mt-2">
+                <img src="images/sci-program/day3.png?v={{ time() }}" class="img-fluid animated" alt="">
+            </div>
+        </div><!-- End Section Title -->
+        
+        
         {{-- <div class="container pt-5 mt-5">
             <img src="images/sci-program/front.png" class="img-fluid animated" alt="">
         </div> --}}
-        <div class="container pt-3 mt-2">
-            <img src="images/sci-program/day1.png?v={{ time() }}" class="img-fluid animated" alt="">
-        </div>
-        <div class="container pt-sm-5 mt-2">
-            <img src="images/sci-program/day2.png?v={{ time() }}" class="img-fluid animated" alt="">
-        </div>
-        <div class="container pt-sm-5 mt-2">
-            <img src="images/sci-program/day3.png?v={{ time() }}" class="img-fluid animated" alt="">
-        </div>
+        
         
     </section>
 @endsection
